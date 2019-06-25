6d98df88f212191da7a83df19ecaeb024351fefd
Josh Minch <josh.minch@gmail.com>
2019-06-24 18:40:43 -0700

    deleted obsolete modules, food const working

--- nutrient.py
+++
@@ -1,7 +0,0 @@
-class Nutrient:
-    def __init__(self, name, id = None, lower_req = None, target_req = None, upper_req = None):
-        self.name = name
-        self.id = id
-        self.lower_req = lower_req
-        self.target_req = target_req
-        self.upper_req = upper_req
--- spartan.py
+++ spartan.py
@@ -42,7 +42,7 @@ class Person:
         self.nuts = [nut for nut in self.nuts if nut.name not in nut_name] 
    
     def add_foods(self, food_ids, prices=None):
-        for food_id, price in food_ids, prices:
+        for food_id, price in zip(food_ids, prices):
             self.foods.append(Food(food_id, price=price))
         self.foods.sort(key = lambda f: f.id)
 
@@ -91,11 +91,14 @@ class Nutrient:
 
 class Optimizier:
     def __init__(self):
+        self.lp_prob = None
+        self.food_quantity_vector = None
+        self.nutrition_matrix = None
         self.optimum_solution = None
-
+    
     def make_nutrition_matrix(self, person):
         nut_ids = [nut.id for nut in person.nuts]
-        food_ids = person.food_ids
+        food_ids = [food.id for food in person.foods]
 
         con = sql.connect('sr28.db')
         cur = con.cursor()
@@ -108,86 +111,81 @@ class Optimizier:
         (''' + (len(nut_ids) - 1) * '?, ' + '?) order by food_id, nut_id''', food_ids + nut_ids)
 
         unformatted_data = cur.fetchall()
-        nutrition_matrix = self.format_nutrition_matrix(unformatted_data, person)
-        return nutrition_matrix
-
+        self.nutrition_matrix = self.format_nutrition_matrix(unformatted_data, person)
+        
     def format_nutrition_matrix(self, unformatted_data, person):
-        nutrients = person.nuts
-        food_ids = person.food_ids
-
         # Construct formatted matrix where each row is a food and each col is a nutrient
         unformatted_data = np.array([float(d[0]) for d in unformatted_data])
-        unformatted_data = unformatted_data.reshape(len(food_ids), len(nutrients))
+        unformatted_data = unformatted_data.reshape(len(person.foods), len(person.nuts))
 
         formatted_data = np.transpose(unformatted_data)
         return formatted_data
 
     def construct_lp_problem(self, person):
-        prob = LpProblem("Diet", sense=LpMinimize)
+        self.lp_prob = LpProblem("Diet", sense=LpMinimize)
 
         food_ids = [food.id for food in person.foods]
         prices = [food.price for food in person.foods]
-    
-        food_amount = np.array(
+
+        self.food_quantity_vector = np.array(
         [LpVariable(str(id), 0, None, LpContinuous) for id in food_ids])
-        prob += lpSum(prices * food_amount), "Total cost of foods"
+
+        self.lp_prob += lpSum(prices * self.food_quantity_vector), "Total cost of foods"
 
     def add_food_constraints(self, person):
-        lower_reqs = [food.lower_req for food in person.foods]
-        target_reqs = [food.target_req for food in person.foods]
-        upper_reqs = [food.upper_req for food in person.foods]
 
         # Add food quantity constraint
         #food_constraints = [f for f in food_amount if f.name in ['1077', '15265']]
         #for food in food_constraints:
-        #    prob += food == 2
+        #    self.lp_prob += food == 2
 
         # Add cost constraint
-        #prob += lpSum(prices * food_amount) >= 1000 / 100
+        #self.lp_prob += lpSum(prices * food_amount) >= 1000 / 100
 
         # Add food quantity constraint
-        #prob += food_amount[0] >= 100 / 100
+        #self.lp_prob += food_amount[0] >= 100 / 100
 
-        for food in person.foods:
-            
-        for i in range(len(food_amount)):
-            prob += food_amount[i] >= 0
+        for i, food in enumerate(person.foods):
+            self.lp_prob += self.food_quantity_vector[i] >= 0
+            if food.lower_req is not None:
+                self.lp_prob += self.food_quantity_vector[i] >= food.lower_req
+            if food.target_req is not None:
+                self.lp_prob += self.food_quantity_vector[i] == food.target_req
+            if food.upper_req is not None:
+                self.lp_prob += self.food_quantity_vector[i] <= food.upper_req
 
     def add_nutrient_constraints(self, person):
         lower_reqs = [nut.lower_req for nut in person.nuts]
         target_reqs = [nut.target_req for nut in person.nuts]
         upper_reqs = [nut.upper_req for nut in person.nuts]
 
-        for i in range(len(formatted_data)):
+        for i in range(len(self.nutrition_matrix)):
             if lower_reqs[i] is not None:
-                prob += lpSum(formatted_data[i] * food_amount) >= lower_reqs[i]
+                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) >= lower_reqs[i]
             if target_reqs[i] is not None:
-                prob += lpSum(formatted_data[i] * food_amount) == target_reqs[i]
+                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) == target_reqs[i]
             if upper_reqs[i] is not None:
-                prob += lpSum(formatted_data[i] * food_amount) <= upper_reqs[i]
+                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) <= upper_reqs[i]
 
     def optimize_diet(self, person):
         
-        nutrition_matrix = self.make_nutrition_matrix(person)
+        self.make_nutrition_matrix(person)
+        self.construct_lp_problem(person)
+        self.add_nutrient_constraints(person)
+        self.add_food_constraints(person)
 
-        self.construct_lp_problem()
-        self.add_nutrient_constraints()
-        self.add_food_constraints()
+        self.lp_prob.writeLP("DietModel.lp")
+        self.lp_prob.solve()
+        
+    def describe_solution(self):
 
-        prob.writeLP("DietModel.lp")
-        prob.solve()
+        print("Status: " + LpStatus[self.lp_prob.status])
 
-        return prob
-
-    def describe_solution(prob, Person):
-
-        print("Status: " + LpStatus[prob.solve()])
-
-        for v in prob.variables():
+        for v in self.lp_prob.variables():
             if (v.varValue != 0):
                 print(query.get_food_name(v.name), "=", 100 * v.varValue)
 
-        print(100 * value(prob.objective), "total grams of food")
+        print(100 * value(self.lp_prob.objective), "total grams of food")
 
 def add_random_foods():
     con = sql.connect('sr28.db')
@@ -198,13 +196,10 @@ def add_random_foods():
 
     food_ids = [f[0] for f in food_list]
 
-    food_ids.sort()
+    return food_ids.sort()
 
 def main():
     josh = Person(25, 'm')
-    
-    '''for f in food_ids:
-        print( str(format(f).zfill(MAX_FOOD_ID_LEN)) + " - " + query.get_food_name(f) )'''
 
     food_ids = basic_foods.food
 
@@ -212,34 +207,27 @@ def main():
         for f in food_ids:
             file.write(str(format(f).zfill(MAX_FOOD_ID_LEN)) + " - " + query.get_food_name(f) + '\n')
     
-    prices =  1 * np.ones(len(food_ids))
+    prices = len(food_ids) * [1]
     josh.add_foods(food_ids, prices)
+    
+    for food in josh.foods[:5]:
+        food.lower_req = 1
 
     josh.remove_nut(['fl'])
 
     josh.add_nut(Nutrient('DHA', 631, lower_req = 0.3))
     josh.add_nut(Nutrient('EPA', 629, lower_req = 0.3))
-    josh.add_nut(Nutrient('energy', 208, lower_req=2000, upper_req=2500))
+    josh.add_nut(Nutrient('energy', 208, lower_req=2000))
     josh.add_nut(Nutrient('sat', 606, upper_req=27))
 
-    optimizer = Optimizier()
+    optimizier = Optimizier()
+    start_time = time.time()
     optimizier.optimize_diet(josh)
-
+    print("--- %s seconds ---" % (time.time() - start_time))
     optimizier.describe_solution()
-    query_time = time.time()
-    unformatted_data = query_database(josh)
-    print("--- %s seconds query ---" % (time.time() - query_time))
-    
-    formatted_data = shape_data(unformatted_data, josh)
-
-    opt_time = time.time()
-    prob = optimize_diet(formatted_data, josh)
-    print("--- %s seconds opt ---" % (time.time() - opt_time))
-
-    describe_solution(prob, josh)
-
+   
 
 if __name__ == '__main__':
-    start_time = time.time()
+   
     main()
-    print("--- %s seconds ---" % (time.time() - start_time))
+   
