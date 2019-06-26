56c143f71b66355e9d4c6cf7da2146725594a008
Josh Minch <josh.minch@gmail.com>
2019-06-25 20:15:36 -0700

    Add or remove food by id or name

--- spartan.py
+++ spartan.py
@@ -27,7 +27,7 @@ class Person:
         nuts = [ Nutrient(name, id, lower_req) for (id, name, lower_req) 
         in zip(req_data.nut_ids, req_data.nut_names, req_data.lower_req[(self.age_range, self.sex)])]
         
-        nuts.sort(key=lambda x: x.id)
+        nuts.sort(key=lambda nut: nut.nut_id)
         return nuts
 
     def add_nut(self, nutrient):
@@ -36,25 +36,34 @@ class Person:
             nutrient.id = req_data.nut_ids[nut_index]
 
         self.nuts.append(nutrient)
-        self.nuts.sort(key=lambda x: x.id)
+        self.nuts.sort(key=lambda nut: nut.nut_id)
         
     def remove_nut(self, nut_name):
         self.nuts = [nut for nut in self.nuts if nut.name not in nut_name] 
    
-    def add_foods(self, food_ids, prices=None):
-        for food_id, price in zip(food_ids, prices):
-            self.foods.append(Food(food_id, price=price))
-        self.foods.sort(key = lambda f: f.id)
+    def add_foods(self, food_names=None, food_ids=None):
+        if food_names is not None:
+            for name in food_names:
+                self.foods.append(Food(name=name))
+        elif food_ids is not None:
+            for food_id in food_ids:
+                self.foods.append(Food(food_id=food_id))
+       
+        self.foods.sort(key = lambda f: f.food_id)
+
+    def set_food_price(self, food_id, price):
+        food = next(food for food in self.foods if food.id == food_id)
+        food.price = price
 
     def remove_foods(self, food_name):
         self.foods = [food for food in self.foods if food.name not in food_name] 
 
 
 class Food:
-    def __init__(self, id, price=None, name=None, lower_req=None, target_req=None, upper_req=None):
-        self.id = id
+    def __init__(self, food_id=None,  name=None, price=None, lower_req=None, target_req=None, upper_req=None):
+        self.food_id = food_id or self.get_food_id(name)
+        self.name = name or self.get_food_name(food_id)
         self.price = price
-        self.name = name or self.get_food_name()
         self.lower_req = lower_req
         self.target_req = target_req
         self.upper_req = upper_req
@@ -62,7 +71,7 @@ class Food:
     def get_nutrition(self):
         con = sql.connect('sr28.db')
         cur = con.cursor()
-        cur.execute("SELECT nut_value FROM nut_data where food_id = ?", [self.id])
+        cur.execute("SELECT nut_value FROM nut_data where food_id = ?", [self.food_id])
         nut_values = cur.fetchall()
 
         for i in range(len(nut_values)):
@@ -70,21 +79,26 @@ class Food:
 
         return nut_values
 
-    def get_food_name(self):
+    def get_food_name(self, food_id):
         con = sql.connect('sr28.db')
         cur = con.cursor()
-        cur.execute("SELECT food_name FROM food_des where food_id = ?", [self.id])
-        food_name = cur.fetchall()
-
-        for i in range(len(food_name)):
-            food_name = food_name[i][0]
+        cur.execute("SELECT food_name FROM food_des WHERE food_id = ?", [food_id])
+        food_name = cur.fetchall()[0][0]
 
         return food_name
+    
+    def get_food_id(self, food_name):
+        con = sql.connect('sr28.db')
+        cur = con.cursor()
+        cur.execute("SELECT food_id FROM food_des WHERE food_name = ?", [food_name])
+        food_id = cur.fetchall()[0]
+
+        return food_id
 
 class Nutrient:
-    def __init__(self, name, id=None, lower_req=None, target_req=None, upper_req=None):
+    def __init__(self, name, nut_id=None, lower_req=None, target_req=None, upper_req=None):
         self.name = name
-        self.id = id
+        self.nut_id = nut_id
         self.lower_req = lower_req
         self.target_req = target_req
         self.upper_req = upper_req
@@ -208,7 +222,9 @@ def main():
             file.write(str(format(f).zfill(MAX_FOOD_ID_LEN)) + " - " + query.get_food_name(f) + '\n')
     
     prices = len(food_ids) * [1]
-    josh.add_foods(food_ids, prices)
+    josh.add_foods(food_ids=food_ids)
+    for food in food_ids:
+        josh.set_food_price(food, 1)
     
     for food in josh.foods[:5]:
         food.lower_req = 1
@@ -231,3 +247,23 @@ if __name__ == '__main__':
    
     main()
    
+
+'''
+Structure I'm thinking of
+
+    input = get_user_input()
+    user = Person()
+    user.name = input.name
+    user.lifestage = user.lifestage 
+    user.select_nutrients
+        user.calculate_req()
+    
+    groc_store = GroceryStore('stater_bros')
+    fridge = Fridge(Person)
+    groc_store.groceries = input.groceries
+    fridge.groceries = GroceryStore.groceries
+
+    unformatted_data = query_nutritional_database(fridge, user)  //
+    formatted_data = format_data(unformatted_data, fridge, user)
+    optimize_diet(formatted_data, fridge, user)
+'''
