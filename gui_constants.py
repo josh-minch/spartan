7a7c63ebc57c843adc1afab630534a025b23c93d
Josh Minch <josh.minch@gmail.com>
2019-07-01 21:27:45 -0700

    Put constants in own file

---
+++ gui_constants.py
@@ -0,0 +1,9 @@
+NAME_COL = 0
+PRICE_COL = 1
+MIN_COL = 2
+QUANTITY_COL = 2
+MAX_COL = 3
+TARGET_COL = 4
+
+col_to_attr = {NAME_COL: 'name', PRICE_COL: 'price',
+               MIN_COL: 'min', MAX_COL: 'max', TARGET_COL: 'target'}
