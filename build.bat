cda0fd15f74a6d7fa793258037f50aeff74538f9
Josh Minch <josh.minch@gmail.com>
2019-07-30 15:08:35 -0700

    Add search window to build

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 pyside2-uic ui_mainwindow.ui > ui_mainwindow.py
+pyside2-uic ui_searchwindow.ui > ui_searchwindow.py
 pyside2-uic ui_optimum_diet_window.ui > ui_optimum_diet_window.py
 python gui.py
