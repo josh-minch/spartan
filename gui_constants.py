7ca09502b5318280d74f35bcdcfde3a941daa8c5
Josh Minch <josh.minch@gmail.com>
2019-08-01 20:36:43 -0700

    Fix off by 1 value

--- gui_constants.py
+++ gui_constants.py
@@ -5,8 +5,8 @@ MIN_COL = 3
 MAX_COL = 4
 TARGET_COL = 5
 
-OPT_NAME_COL = 1
-OPT_PRICE_COL = 2
+OPT_NAME_COL = 0
+OPT_PRICE_COL = 1
 OPT_QUANTITY_COL= 2
 
 col_to_attr = {FOOD_ID_COL: 'food_id', NAME_COL: 'name', PRICE_COL: 'price',
