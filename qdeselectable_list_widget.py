3967a4802d5747856f7b7d4383821c5ae4dac6a8
Josh Minch <josh.minch@gmail.com>
2019-08-27 18:28:43 -0700

    Delete obsolete and replaced modules

--- qdeselectable_list_widget.py
+++
@@ -1,8 +0,0 @@
-from PySide2 import QtCore, QtWidgets, QtGui
-
-class QDeselectableListWidget(QtWidgets.QListWidget):
-    def mousePressEvent(self, event):
-        item = self.indexAt(event.pos())
-        if (item.isValid() is not True):
-            self.clearSelection()
-        QtWidgets.QListWidget.mousePressEvent(self, event)
--- qdeselectable_table_widget.py
+++
@@ -1,8 +0,0 @@
-from PySide2 import QtCore, QtWidgets, QtGui
-
-class QDeselectableTableWidget(QtWidgets.QTableWidget):
-    def mousePressEvent(self, event):
-        item = self.indexAt(event.pos())
-        if (item.isValid() is not True):
-            self.clearSelection()
-        QtWidgets.QTableWidget.mousePressEvent(self, event)
