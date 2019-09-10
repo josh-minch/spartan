81bac9b42b0c53d7fd31234f7d590ef20e23dbaa
Josh Minch <josh.minch@gmail.com>
2019-09-10 16:05:54 -0700

    Fix optimization bugs related to sorting of foods

--- spartan.py
+++ spartan.py
@@ -31,25 +31,20 @@ class Person(object):
     def set_nuts(self):
         (macro, vit, mineral) = req.get_req(self.age_range, self.sex)
 
-        nuts = []
+        self.nuts = []
+
         for nutrient in macro + vit + mineral:
             # TODO: restructure data to fix this mess
+            # Get nut_id from nut name
             nut_id = list(req.nuts.keys())[list(req.nuts.values()).index(nutrient['name'])]
             nut_to_append = Nutrient(name=nutrient['name'], nut_id=nut_id, min=nutrient['min'])
-            nuts.append(nut_to_append)
+            self.nuts.append(nut_to_append)
 
-        nuts.sort(key=lambda nut: nut.nut_id)
-        self.nuts = nuts
         self.remove_nut('Fluoride (F)')
-        #self.remove_nut('Fat')
-        #self.add_nut(Nutrient('Energy', 208, min=2300, max=2600))
-        #self.add_nut(Nutrient('Sugars, total'))
+        self.add_nut(Nutrient('Energy', nut_id=208, min=2000, max=2900))
+        self.nuts.sort(key=lambda nut: nut.nut_id)
 
     def add_nut(self, nutrient):
-        if (nutrient.nut_id == None):
-            nut_index = req.nut_names.index(nutrient.name)
-            nutrient.nut_id = req.nut_ids[nut_index]
-
         self.nuts.append(nutrient)
         self.nuts.sort(key=lambda nut: nut.nut_id)
 
@@ -136,6 +131,7 @@ class Food:
 
         self.food_id = food_id if food_id is not None else database.get_food_id(name)
         self.name = name or database.get_food_name(food_id)
+
         self.price: float          = price
         self.price_quantity: float = price_quantity
         self.price_unit: str       = price_unit
@@ -161,6 +157,151 @@ class Food:
         cur.execute(sql_stmnt, self.food_id)
 
         return cur.fetchall()
+
+class Nutrient:
+    def __init__(self, name, nut_id=None, min=None, target=None, max=None):
+        self.name = name
+        self.nut_id = nut_id
+        self.min = min
+        self.target = target
+        self.max = max
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+class Optimizier:
+
+    def __init__(self, person):
+        self.lp_prob = LpProblem("Diet", sense=LpMinimize)
+        self.person = person
+
+    def make_nutrition_matrix(self):
+        nut_ids = [nut.nut_id for nut in self.person.nuts]
+        food_ids = [food.food_id for food in self.person.foods]
+
+        con = sql.connect('sr_legacy/sr_legacy.db')
+        cur = con.cursor()
+
+        # Get nutritional values for each of the nutrients for each user's food.
+        # The len(food_ids)-1 are to programmatically generate a SQL statement with
+        # a variable length number of parameters, since food_ids and nut_ids vary depending on user settings
+
+        sql_stmnt = '''select amount from nut_data where food_id in \
+        (''' + (len(food_ids) - 1) * '?, ' + '?) and nut_id in \
+        (''' + (len(nut_ids) - 1) * '?, ' + '?) order by food_id, nut_id'''
+
+        cur.execute(sql_stmnt, food_ids + nut_ids)
+
+        nut_data = cur.fetchall()
+        self.nutrition_matrix = self.format_nutrition_matrix(nut_data)
+
+    def format_nutrition_matrix(self, unformatted_data):
+        # Construct formatted matrix where each row is a food and each col is a nutrient
+        unformatted_data = np.array(unformatted_data)
+        unformatted_data[unformatted_data == None] = 0
+        unformatted_data = unformatted_data.reshape(len(self.person.foods), len(self.person.nuts))
+
+        formatted_data = np.transpose(unformatted_data)
+        return formatted_data
+
+    def construct_lp_problem(self):
+        food_ids = [food.food_id for food in self.person.foods]
+        ## TODO: Require prices on all foods? SETTING PRICE = 1 TEMPORARY FOR TESTING
+        prices = [float(food.price) if food.price is not None else 1 for food in self.person.foods]
+
+        self.food_quantity_vector = np.array(
+            [LpVariable(str(food_id), 0, None, LpContinuous) for food_id in food_ids])
+
+        self.lp_prob += lpSum(prices * self.food_quantity_vector), "Total cost of foods"
+
+    def add_food_constraints(self):
+        for i, food in enumerate(self.person.foods):
+            self.lp_prob += self.food_quantity_vector[i] >= 0
+
+            constraints = [food.min, food.max, food.target]
+            units = [food.min_unit, food.max_unit, food.target_unit]
+            operators = [operator.ge, operator.le, operator.eq]
+
+            for constraint, unit, op in zip(constraints, units, operators):
+                if constraint is not None:
+                    converted_constraint = self.convert_constraint(food.food_id, constraint, unit)
+                    self.add_constraint(self.food_quantity_vector[i], op,
+                                        converted_constraint/DB_SCALER)
+
+    def convert_constraint(self, food_id, constraint_value, constraint_unit):
+        if constraint_unit == 'g':
+            return constraint_value
+        else:
+            return convert_quantity(food_id, constraint_value, constraint_unit, 'g')
+
+    def add_constraint(self, var, comparator, constraint_value):
+        # parameterized <=, >=, and == comparators with operator
+        self.lp_prob += comparator(var, constraint_value)
+
+    def add_nutrient_constraints(self):
+        mins = [nut.min for nut in self.person.nuts]
+        targets = [nut.target for nut in self.person.nuts]
+        maxes = [nut.max for nut in self.person.nuts]
+
+        for i in range(len(self.nutrition_matrix)):
+            if mins[i] is not None:
+                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) >= mins[i]
+            if targets[i] is not None:
+                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) == targets[i]
+            if maxes[i] is not None:
+                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) <= maxes[i]
+
+    def optimize_diet(self):
+        self.person.foods.sort(key=lambda f: f.food_id)
+        self.make_nutrition_matrix()
+        self.construct_lp_problem()
+        self.add_nutrient_constraints()
+        self.add_food_constraints()
+
+        self.lp_prob.writeLP("DietModel.lp")
+        self.lp_prob.solve()
+
+    def get_solution_status(self):
+        status_statement = ""
+
+        if (self.lp_prob.status == LpStatusOptimal):
+            status_statement = "Optimum diet"
+        elif (self.lp_prob.status == LpStatusInfeasible):
+            status_statement = "A diet that meets your current constraints is infeasible"
+
+        return status_statement
+
+    def describe_solution(self):
+        print("Status: " + LpStatus[self.lp_prob.status])
+
+        for v in self.lp_prob.variables():
+            if (v.varValue is not None and v.varValue > 0):
+                print(database.get_food_name(v.name), "=", 100 * v.varValue)
+
+        print(100 * value(self.lp_prob.objective), "total grams of food")
+
+    def get_diet_report(self):
+        prices = [food.price if food.price is not None else 1 for food in self.person.foods]
+
+        foods = []
+        for i, var in enumerate(self.lp_prob.variables()):
+            if (var.varValue is not None and var.varValue > 0):
+                food_name = database.get_food_name(var.name)
+                cost = round(float(prices[i]) * var.varValue, 2)
+                quantity = round(100 * var.varValue, 2)
+                foods.append({"name":food_name, "cost":cost, "quantity":quantity, "unit":'g'})
+
+        return foods
+
+    def get_totals(self):
+        total_number = len([v for v in self.lp_prob.variables() if v.varValue is not None and v.varValue > 0])
+        total_cost = value(self.lp_prob.objective)
+        running_total_mass = 0
+        for var in self.lp_prob.variables():
+            if (var.varValue is not None and var.varValue > 0):
+                running_total_mass += 100 * var.varValue
+
+        return total_number, total_cost, running_total_mass
 
 def convert_quantity(food_id, quantity, old_unit, new_unit):
 
@@ -192,17 +333,20 @@ def convert_quantity(food_id, quantity, old_unit, new_unit):
     '''
 
 def get_nutrition(person, food_ids, food_amounts):
+    if len(food_ids) == 0:
+        return
+
     con = sql.connect('sr_legacy/sr_legacy.db')
     cur = con.cursor()
 
-    nut_ids = tuple([nut.nut_id for nut in person.nuts])
-    # nutrient list is clean, no need to sanitize
+    nut_ids = [nut.nut_id for nut in person.nuts]
+
     sql_stmt = (
         'SELECT amount '
         'FROM nut_data WHERE food_id IN (' + (len(food_ids) - 1) * '?, ' + '?) '
-        'AND nut_id IN '+ str(nut_ids)
+        'AND nut_id IN (' + (len(nut_ids) - 1) * '?, ' + '?) '
     )
-    cur.execute(sql_stmt, food_ids)
+    cur.execute(sql_stmt, food_ids + nut_ids)
 
     nut_amounts = cur.fetchall()
     nut_amounts = np.array([amount[0] if amount[0] is not None else 0 for amount in nut_amounts])
@@ -238,142 +382,10 @@ def get_nutrition_unit(nut_ids):
     cur = con.cursor()
     sql_stmt = (
         'SELECT units '
-        'FROM nutr_def WHERE id IN '+ str(nut_ids)
+        'FROM nutr_def WHERE id IN (' + (len(nut_ids) - 1) * '?, ' + '?) '
     )
-    cur.execute(sql_stmt)
+    cur.execute(sql_stmt, nut_ids)
     return cur.fetchall()
-
-class Nutrient:
-    def __init__(self, name, nut_id=None, min=None, target=None, max=None):
-        self.name = name
-        self.nut_id = nut_id
-        self.min = min
-        self.target = target
-        self.max = max
-
-    def __repr__(self):
-        return str(self.__dict__)
-
-class Optimizier:
-
-    def __init__(self):
-        self.lp_prob = LpProblem("Diet", sense=LpMinimize)
-
-    def make_nutrition_matrix(self, person):
-        nut_ids = [nut.nut_id for nut in person.nuts]
-        food_ids = [food.food_id for food in person.foods]
-
-        con = sql.connect('sr_legacy/sr_legacy.db')
-        cur = con.cursor()
-
-        # Get nutritional values for each of the nutrients for each user's food.
-        # The len(food_ids)-1 are to programmatically generate a SQL statement with
-        # a variable length number of parameters, since food_ids and nut_ids vary depending on user settings
-
-        sql_stmnt = '''select amount from nut_data where food_id in \
-        (''' + (len(food_ids) - 1) * '?, ' + '?) and nut_id in \
-        (''' + (len(nut_ids) - 1) * '?, ' + '?) order by food_id, nut_id'''
-
-        cur.execute(sql_stmnt, food_ids + nut_ids)
-
-        nut_data = cur.fetchall()
-        self.nutrition_matrix = self.format_nutrition_matrix(nut_data, person)
-
-    def format_nutrition_matrix(self, unformatted_data, person):
-        # Construct formatted matrix where each row is a food and each col is a nutrient
-        unformatted_data = np.array(unformatted_data)
-        unformatted_data[unformatted_data == None] = 0
-        unformatted_data = unformatted_data.reshape(len(person.foods), len(person.nuts))
-
-        formatted_data = np.transpose(unformatted_data)
-        return formatted_data
-
-    def construct_lp_problem(self, person):
-
-        food_ids = [food.food_id for food in person.foods]
-        ## TODO: Require prices on all foods? SETTING PRICE = 1 TEMPORARY FOR TESTING
-        prices = [float(food.price) if food.price is not None else 1 for food in person.foods]
-
-        self.food_quantity_vector = np.array(
-            [LpVariable(str(food_id), 0, None, LpContinuous) for food_id in food_ids])
-
-        self.lp_prob += lpSum(prices * self.food_quantity_vector), "Total cost of foods"
-
-    def add_food_constraints(self, person):
-        for i, food in enumerate(person.foods):
-            self.lp_prob += self.food_quantity_vector[i] >= 0
-
-            constraints = [food.min, food.max, food.target]
-            units = [food.min_unit, food.max_unit, food.target_unit]
-            operators = [operator.ge, operator.le, operator.eq]
-
-            for constraint, unit, op in zip(constraints, units, operators):
-                if constraint is not None:
-                    converted_constraint = self.convert_constraint(food.food_id, constraint, unit)
-                    self.add_constraint(self.food_quantity_vector[i], op,
-                                        converted_constraint/DB_SCALER)
-
-    def convert_constraint(self, food_id, constraint_value, constraint_unit):
-        if constraint_unit == 'g':
-            return constraint_value
-        else:
-            return convert_quantity(food_id, constraint_value, constraint_unit, 'g')
-
-    def add_constraint(self, var, comparator, constraint_value):
-        # parameterized <=, >=, and == comparators with operator
-        self.lp_prob += comparator(var, constraint_value)
-
-    def add_nutrient_constraints(self, person):
-        mins = [nut.min for nut in person.nuts]
-        targets = [nut.target for nut in person.nuts]
-        maxes = [nut.max for nut in person.nuts]
-
-        for i in range(len(self.nutrition_matrix)):
-            if mins[i] is not None:
-                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) >= mins[i]
-            if targets[i] is not None:
-                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) == targets[i]
-            if maxes[i] is not None:
-                self.lp_prob += lpSum(self.nutrition_matrix[i] * self.food_quantity_vector) <= maxes[i]
-
-    def optimize_diet(self, person):
-        self.make_nutrition_matrix(person)
-        self.construct_lp_problem(person)
-        self.add_nutrient_constraints(person)
-        self.add_food_constraints(person)
-
-        self.lp_prob.writeLP("DietModel.lp")
-        self.lp_prob.solve()
-
-    def describe_solution_status(self):
-        status_statement = ""
-
-        if (self.lp_prob.status == LpStatusOptimal):
-            status_statement = "Optimum diet"
-        elif (self.lp_prob.status == LpStatusInfeasible):
-            status_statement = "A diet that meets your current constraints is infeasible"
-
-        return status_statement
-
-    def describe_solution(self):
-        print("Status: " + LpStatus[self.lp_prob.status])
-
-        for v in self.lp_prob.variables():
-            if (v.varValue is not None and v.varValue > 0):
-                print(database.get_food_name(v.name), "=", 100 * v.varValue)
-
-        print(100 * value(self.lp_prob.objective), "total grams of food")
-
-    def get_totals(self):
-        total_number = len([v for v in self.lp_prob.variables() if v.varValue is not None and v.varValue > 0])
-        total_cost = value(self.lp_prob.objective)
-        running_total_mass = 0
-        for var in self.lp_prob.variables():
-            if (var.varValue is not None and var.varValue > 0):
-                running_total_mass += 100 * var.varValue
-
-        return total_number, total_cost, running_total_mass
-
 
 def add_random_foods():
     con = sql.connect('sr_legacy/sr_legacy.db')
