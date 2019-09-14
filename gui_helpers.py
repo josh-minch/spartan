85d236e1421d72204b6a70700d1729b7b9928dc7
Josh Minch <josh.minch@gmail.com>
2019-09-13 20:27:49 -0700

    Add column width setter

--- gui_helpers.py
+++ gui_helpers.py
@@ -6,6 +6,10 @@ def hide_view_cols(view, cols_to_hide):
     for col in cols_to_hide:
         view.setColumnHidden(col, True)
 
+def set_column_widths(view, cols, col_widths):
+    for col, width in zip(cols, col_widths):
+        view.setColumnWidth(col, width)
+
 def set_header_weight(header, weight):
     header_font = QFont()
     header_font.setWeight(weight)
@@ -20,24 +24,17 @@ def setup_table_header(table, labels):
     header = table.horizontalHeader()
     header.setSectionResizeMode(0, QHeaderView.Stretch)
 
-    # Don't call QHeaderView::setSectionResizeMode() for every column. To automatically apply the passed stretch to all columns, 
-    # just call that method once without iteratively passing an explicit column index: 
-    # e.g., ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);. 
-    # The below for loop thus reduces to a simplistic one-liner. 
+    # Don't call QHeaderView::setSectionResizeMode() for every column. To automatically apply the passed stretch to all columns,
+    # just call that method once without iteratively passing an explicit column index:
+    # e.g., ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);.
+    # The below for loop thus reduces to a simplistic one-liner.
     # See also thttps://stackoverflow.com/questions/18293403/columns-auto-resize-to-size-of-qtableview/34190094#34190094
 
     for i in range(1, len(labels)):
         header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
 
-    header.setDefaultAlignment(Qt.AlignLeft) 
+    header.setDefaultAlignment(Qt.AlignLeft)
 
     header_font = QFont()
     header_font.setWeight(QFont.DemiBold)
     header.setFont(header_font)
-
-nut_model_mac_names = {'Energy', 'Carbohydrates', 'Starch', 'Sugars', 'Fiber', 'Protein', 
-                        'Fats', 'Satured fats', 'Monounsaturated fats', 
-                        'Polyunsaturated fats', 'Omega-3', 'EPA', 'DPA', 'DHA', 'Omega-6', 'Trans fats', 'Cholesterol'}
-nut_model_vit_names = {'A', 'B1', 'B2', 'B3', 'B5', 'B6', 'B7', 'B9', 'B12', 'C', 'D', 'E', 'K'}
-nut_model_min_names = {'Calcium', 'Copper', 'Iron', 'Magnesium', 'Manganese',
-                        'Phosphorus', 'Potassium', 'Selenium', 'Sodium', 'Zinc'}
