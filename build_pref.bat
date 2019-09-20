b90b235da5765c978ba5eb341981dbd8cd2f1584
Josh Minch <josh.minch@gmail.com>
2019-09-20 15:15:54 -0700

    Add image resource and build command

--- build_pref.bat
+++ build_pref.bat
@@ -2,5 +2,5 @@ pyside2-uic ui/ui_prefwidget.ui > ui/ui_prefwidget.py
 pyside2-uic ui/ui_reqwidget.ui > ui/ui_reqwidget.py
 pyside2-uic ui/ui_reswidget.ui > ui/ui_reswidget.py
 pyside2-uic ui/ui_prefwindow.ui > ui/ui_prefwindow.py
-
-python pref_window.py
+pyside2-rcc images/images.qrc -o images_rc.py
+pause
