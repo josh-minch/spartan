4935b9b56308bf8777b8b852d2a70f1c308e050c
Josh Minch <josh.minch@gmail.com>
2019-09-25 15:59:22 -0700

    Add new widgets to build

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
 pyside2-uic ui/ui_mainwindow.ui > ui/ui_mainwindow.py
 pyside2-uic ui/ui_searchwindow.ui > ui/ui_searchwindow.py
 pyside2-uic ui/ui_optimumdietwindow.ui > ui/ui_optimumdietwindow.py
-pyside2-uic ui/ui_requirementswindow.ui > ui/ui_requirementswindow.py
+pyside2-uic ui/ui_prefwidget.ui > ui/ui_prefwidget.py
+pyside2-uic ui/ui_reqwidget.ui > ui/ui_reqwidget.py
+pyside2-uic ui/ui_reswidget.ui > ui/ui_reswidget.py
+pyside2-uic ui/ui_prefwindow.ui > ui/ui_prefwindow.py
+
+pyside2-rcc images/images.qrc -o images_rc.py
+
 python gui.py
